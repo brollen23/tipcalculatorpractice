ad5128819aa80b094eda84600cbbdd19ce8b28f3
nwashington <nikolee1541@gmail.com>
2022-07-15 12:08:07 -0400

    work from class added

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let billAmount = document.querySelector("#billA");
+let totalNumG = document.querySelector("#numOfG");
+let tipP = document.querySelector("#tipP");
+let tipAmout = document.querySelector("#tipA");
+let totalAmount = document.querySelector("#totalA");
+let buttons= document.querySelectorAll(".tipButton");
+
+function tipButtonHandler(event){
+    let button = event.currentTarget;
+    tipAmout = parseFloat(button.innerText);
+    document.querySelector("#tipP").value = tipAmout;
+}
+for(let i=0; i<buttons.length; i++){
+    buttons[i].addEventListener("click", tipButtonHandler);
+}
