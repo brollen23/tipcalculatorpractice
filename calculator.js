d9e9aeeba08c8939a5770619779265f7edc19349
lbaynard <lbaynard98@gmail.com>
2022-07-17 12:58:44 -0400

    accepted changes

--- calculator.js
+++ calculator.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-let billAmount = document.querySelector("#billA");
-let totalNumG = document.querySelector("#numOfG");
-let tipP = document.querySelector("#tipP");
-let tipAmout = document.querySelector("#tipA");
-let totalAmount = document.querySelector("#totalA");
-let buttons= document.querySelectorAll(".tipButton");
-
-function tipButtonHandler(event){
-    let button = event.currentTarget;
-    tipAmout = parseFloat(button.innerText);
-    document.querySelector("#tipP").value = tipAmout;
-}
-for(let i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click", tipButtonHandler);
-}
-=======
 let billAmount = document.querySelector("#billA");
 let totalNumG = document.querySelector("#numOfG");
 let tipP = document.querySelector("#tipP");
@@ -36,4 +19,3 @@ document.getElementById("calculate").onClick = function(){
 
 // //Hide the tip amount until calculate button is pressed
 // document.getElementById("tipA").style.display = "none";
->>>>>>> f7d7a14caae1e7b5a74bcf9a28224c065f6d1bac
