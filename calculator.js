800c8a99bb91cb17b85b4274ca42c6c9cf5b71ae
nwashington <nikolee1541@gmail.com>
2022-07-17 23:18:02 -0400

    made changes to JS

--- calculator.js
+++ calculator.js
@@ -1,15 +1,26 @@
 let billAmount = document.querySelector("#billA");
 let totalNumG = document.querySelector("#numOfG");
 let tipP = document.querySelector("#tipP");
-let tipAmout = document.querySelector("#tipA");
+let tipAmount = document.querySelector("#tipA");
 let totalAmount = document.querySelector("#totalA");
 let buttons= document.querySelectorAll(".tipButton");
+let button = document.querySelector("#btn");
+
+button.addEventListener("click", function(){
+    billAmount = document.querySelector("#billA").value;
+    tipP = document.querySelector("#tipP").value;
+    tipAmount = document.querySelector("#tipA").value= parseFloat(billAmount)/ tipP;
+    totalAmount = document.querySelector("#totalA").value = parseFloat(billAmount) + parseFloat(tipAmount).toFixed(2);
+})
+
+
 
 function tipButtonHandler(event){
     let button = event.currentTarget;
-    tipAmout = parseFloat(button.innerText);
-    document.querySelector("#tipP").value = tipAmout;
+    tipAmount = parseFloat(button.innerText);
+    document.querySelector("#tipP").value = tipAmount;
 }
 for(let i=0; i<buttons.length; i++){
     buttons[i].addEventListener("click", tipButtonHandler);
 }
+
